7a87b434679444a9e3e1916d53ac87a932e49b5e
CamilaRanniele <rannielecamsil@gmail.com>
2022-07-01 18:50:40 -0300

    feat: rota para atualizar uma task

--- back-end/src/routes/Task.routes.js
+++ back-end/src/routes/Task.routes.js
@@ -6,5 +6,6 @@ const routes = express.Router();
 routes.get('/', TaskController.getAllTasks);
 routes.post('/', TaskController.createNewTask);
 routes.delete('/:id', TaskController.deleteTask);
+routes.put('/:id', TaskController.updateTask);
 
 module.exports = routes;
