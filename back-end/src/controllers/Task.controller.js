7b22543ac073b264f69bca24f76bc4cbc2fbfee2
CamilaRanniele <rannielecamsil@gmail.com>
2022-07-01 18:50:16 -0300

    feat: funcao que atualiza task na camada controller

--- back-end/src/controllers/Task.controller.js
+++ back-end/src/controllers/Task.controller.js
@@ -18,8 +18,16 @@ const deleteTask = async (req, res) => {
   return res.status(204).end();
 }
 
+const updateTask = async (req, res) => {
+  const { id } = req.params;
+  const { task, status } = req.body;
+  const updatedTask = await TaskService.updateTask({ id, task, status });
+  return res.status(200).json(updatedTask);
+}
+
 module.exports = {
   getAllTasks,
   createNewTask,
   deleteTask,
+  updateTask,
 };
