a9cba4d8d437aa96d83e7e16b9c063e5a3740981
CamilaRanniele <rannielecamsil@gmail.com>
2022-07-01 18:16:33 -0300

    feat: funcoes de listar e cadastrar nova task na camada controller

---
+++ back-end/src/controllers/Task.controller.js
@@ -0,0 +1,17 @@
+const TaskService = require('../services/Task.service');
+
+const getAllTasks = async (req, res) => {
+  const tasks = await TaskService.getAllTasks();
+  return res.status(200).json(tasks);
+}
+
+const createNewTask = async (req, res) => {
+  const { task, status } = req.body;
+  const newTask = await TaskService.createNewTask({ task, status});
+  return res.status(201).json(newTask);
+}
+
+module.exports = {
+  getAllTasks,
+  createNewTask
+};
