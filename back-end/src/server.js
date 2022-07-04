9c9e1e3600650b1c4d02bf7578beb45a45c48d69
CamilaRanniele <rannielecamsil@gmail.com>
2022-07-04 12:08:47 -0300

    feat: adiciona cors para liberar acesso a api

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors')
 const app = express();
 
 const PORT = process.env.SERVER_PORT || 3001;
+app.use(cors());
 app.use(express.json());
 
 app.use('/tasks', routes.TaskRoutes);
+
 
 app.listen(PORT, () => console.log(` Rodando na porta ${PORT}`));
