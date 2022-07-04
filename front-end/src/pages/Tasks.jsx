40d06295a9cbee6c9d00e8c39e2766c3aaadf696
CamilaRanniele <rannielecamsil@gmail.com>
2022-07-04 12:13:53 -0300

    feat: pagina de tasks

--- front-end/src/pages/Tasks.jsx
+++ front-end/src/pages/Tasks.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import TasksTable from '../components/TasksTable';
 
 class Tasks extends React.Component {
   render() {
     return (
       <div>
         Tasks
+        <TasksTable />
       </div>
     )
   }
