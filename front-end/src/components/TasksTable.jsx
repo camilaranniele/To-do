0d7f0aea0aa7cfc2525b2b8578b4625404a359be
CamilaRanniele <rannielecamsil@gmail.com>
2022-07-04 14:16:58 -0300

    feat: criar nova tarefa

--- front-end/src/components/TasksTable.jsx
+++ front-end/src/components/TasksTable.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 
 function TasksTable() {
   const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState({task: '', status: 'pendente'});
   const baseURL = 'http://localhost:3001/tasks'
 
   useEffect(() => {
@@ -10,6 +11,25 @@ function TasksTable() {
       setTasks(response.data);
     });
   }, []);
+
+  function handleChange({ target}) {
+    const { name, value } = target;
+    setTask({
+      ...task,
+      [name]: value });
+  }
+
+  function createNewTask() {
+    const teste = axios
+      .post(baseURL, {
+        task: task.task,
+        status: task.status,
+      })
+      .then((response) => {
+        setTask([...tasks, task])
+      })
+      console.log(teste);
+  }
 
   function deleteTask(id) {
      axios
@@ -22,6 +42,37 @@ function TasksTable() {
 
   return (
     <div>
+      <form>
+        <label htmlFor="task">
+          <input
+            type="text"
+            name="task"
+            onChange={ handleChange }
+            value={ task.task }
+          />
+        </label>
+
+        <label htmlFor="status">
+          <select
+            id="status"
+            name="status"
+            onChange={ handleChange }
+            value={ task.status }
+          >
+            <option value="pendente">
+              pendente
+            </option>
+            <option value="em andamento">
+              em andamento
+            </option>
+            <option value="pronto">
+              pronto
+            </option>
+          </select>
+        </label>
+
+        <button onClick={ createNewTask }>Adicionar</button>
+      </form>
       <table>
         <thead>
           <th>Task</th>
